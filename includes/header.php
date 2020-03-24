b4c2ac303c3c04da0d390715b861afc0c58a2daf
eduboz <eduboz@ensc.fr>
2020-03-24 19:14:46 +0100

    24/03 19h dub

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,8 @@
     <nav>        
         <ul>
              
-            <div id="logo">  <p><img src="images/logo1.png" width="80" /></p></div> 
-            <li>MYQUIZZ</li>
+            <div id="logo">  <p><a href="page1.php"><img src="images/logo1.png" width="80" /></a></p></div> 
+            
 
             <li>Quizz par thème
                 <ul class="sous">
@@ -16,9 +16,24 @@
                     <li ><a class="link" href ="histgeo.php">Histoire/Géographie</a></li>
                     <li ><a class="link" href ="maths.php">Maths</a></li>                 
                 </ul>
-            </li>              
-                                     
+            </li> 
+			
+				<?php if (isUserConnected()) { ?>
+					<li> Bienvenue, <?= $_SESSION['login'] ?> <b class="caret"></b>
+						<ul class="sous">
+							<li ><a class="link" href ="profil.php">Mon profil</a></li>
+							<li ><a class="link" href ="logout.php">Déconnexion</a></li>  
+						</ul>
+					</li>
+														
+					<?php } else { ?>
+					<li>Non connecté
+						<ul class="sous">
+							<li><a class="link" href="login.php">Se connecter</a></li>
+						</ul>
+					</li>                                   
         </ul>
+		   <?php } ?>
     </nav>
-	<br/>
+
 </header>
